af42a3143c37c0623ce8b879f771503c4a7f11aa
Rob Speer <rspeer@mit.edu>
2010-04-17 00:21:57 -0400

    initial git commit

---
+++ anagram.py
@@ -0,0 +1,99 @@
+import string
+import cPickle as pickle
+from bag import bag as bagrepr
+
+picklefile = open('anagram_data.pickle')
+ngrams = pickle.load(picklefile)
+picklefile.close()
+
+keylist = ngrams.keys()
+keylist.sort()
+
+def simple_anagram_numeric(bagnum):
+    return ngrams.get(bagnum)
+
+def simple_anagram(text):
+    bagnum = bagrepr(text)
+    return simple_anagram_numeric(bagnum)
+
+def complex_anagram_gen(bagnum):
+    for key in ngrams:
+        if bagnum % key == 0:
+            text, words, freq = ngrams[key]
+            other = simple_anagram_numeric(bagnum/key)
+            if other:
+                othertext, otherwords, otherfreq = other
+                if freq <= otherfreq:
+                    yield text+' '+othertext, words+otherwords, freq*otherfreq
+
+def complex_anagram(text):
+    bagnum = bagrepr(text)
+    firsttry = simple_anagram_numeric(bagnum)
+    if firsttry: return firsttry[0], 1, firsttry[2]
+
+    bestfreq = 0
+    besttext = None
+    for text, words, freq in complex_anagram_gen(bagnum):
+        if freq > bestfreq:
+            besttext, bestfreq = text, freq
+    return besttext, 2, bestfreq
+        
+def multi_anagram(text, n=10):
+    got = []
+    bagnum = bagrepr(text)
+    firsttry = simple_anagram_numeric(bagnum)
+    if firsttry:
+        got.append((-1, firsttry[2], firsttry[0]))
+    for text, words, freq in complex_anagram_gen(bagnum):
+        got.append((-2, freq, text))
+    got.sort()
+    best = []
+    used = set()
+    for i in range(1, len(got)+1):
+        text = got[-i][2]
+        ordered = ' '.join(sorted(text.split()))
+        if ordered not in used:
+            used.add(ordered)
+            best.append(text)
+            if len(used) >= n: break
+    return best
+    
+def demo():
+    print simple_anagram("ogle gobot")
+    print multi_anagram("peach winslow")
+
+def wildcard_anagram(text, n=10):
+    nblanks = text.count('?')
+    if nblanks > 4:
+        return ['That has too many blanks.']
+    strings = [text]
+    for i in range(nblanks):
+        new_strings = [st + c for st in strings for c in string.lowercase]
+        strings = new_strings
+    got = []
+    for anastring in strings:
+        simple = simple_anagram(anastring)
+        if simple:
+            newtext, words, freq = simple
+            got.append((freq, newtext))
+    got.sort()
+    best = []
+    used = set()
+    for i in range(1, len(got)+1):
+        text = got[-i][1]
+        ordered = ' '.join(sorted(text.split()))
+        if ordered not in used:
+            used.add(ordered)
+            best.append(text)
+            if len(used) >= n: break
+    return best
+
+def anagram_dispatch(text):
+    nblanks = text.count('?')
+    if nblanks == 0:
+        return multi_anagram(text)
+    else:
+        return wildcard_anagram(text)
+
+if __name__ == '__main__': demo()
+
