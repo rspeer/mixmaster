c3dfdbb1b54af140d4e226f375dc49ca5ad306f5
Rob Speer <rspeer@mit.edu>
2010-10-28 21:12:41 -0400

    better handling of long anagrams; added things like letter_diff

--- bag_of_letters.py
+++ bag_of_letters.py
@@ -11,11 +11,21 @@ import unittest
 def bag_empty (b):
     return b == 1
 
+
 letters_to_primes = {}
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
 letterfreq = "etaoinshrdlcumwfgypbvkjxqz"
 for let, prime in zip(letterfreq, primes):
     letters_to_primes[let] = prime
+
+def letter_diff(first, second):
+    first_out = ''
+    second_out = ''
+    for let in letterfreq:
+        diff = first.count(let) - second.count(let)
+        if diff > 0: first_out += let*diff
+        elif diff < 0: second_out += let*(-diff)
+    return first_out, second_out
 
 def make_bag (str):
     str = str.lower()
