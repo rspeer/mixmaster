28ebe4dd7cb86281e5f2ecbf9e0dbb3b3b27e8db
Rob Speer <rspeer@mit.edu>
2010-10-30 14:30:58 -0400

    simplify alphagram

--- alphagram.py
+++ alphagram.py
@@ -8,24 +8,10 @@ import string
 import sys
 import unittest
 
-def alpha_empty (b):
+def alpha_empty(b):
     return b == ""
 
-def make_alpha (str):
-    str = str.lower()
-    counts = []
-
-    for i in range(0, 26):
-        counts.append(0)
-
-    for c in str:
-        if (c >= 'a') and (c <= 'z'):
-            counts[ord(c) - ord('a')] += 1
-
-    rv = "";
-    for i in range(0, 26):
-        for j in range(0, counts[i]):
-            rv += (chr(ord('a') + i))
-
-    return rv
+def make_alpha(str):
+    str = str.upper()
+    return ''.join(sorted(c for c in str if c >= 'A' and c <= 'Z'))
 
